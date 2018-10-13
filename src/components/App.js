c9ee3dfe180c5af0d64e5e2ff5f5bb4513d3ea93
sang-bae <sangbae@berkeley.edu>
2018-10-12 23:33:36 -0700

    added Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import Popular from './Popular';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Nav from "./Nav";
+import Home from "./Home";
+import Battle from "./Battle";
+import Popular from "./Popular";
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
-    )
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={function () {
+              return <p>404 Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -25,12 +25,12 @@ function SelectLanguage(props) {
 
 function RepoGrid(props) {
   return (
-    <ul className='popular-list'>
+    <ul className="popular-list">
       {props.repos.map(function(repo, index) {
         return (
-          <li key={repo.name} className='popular-item'>
-            <div className='popular-rank'>#{index + 1}</div>
-            <ul className='space-list-items'>
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rank">#{index + 1}</div>
+            <ul className="space-list-items">
               <li>
                 <img
                   className="avatar"
@@ -38,7 +38,9 @@ function RepoGrid(props) {
                   alt={"Avatar for " + repo.owner.login}
                 />
               </li>
-              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
               <li>@{repo.owner.login}</li>
               <li>{repo.stargazers_count} stars</li>
             </ul>
